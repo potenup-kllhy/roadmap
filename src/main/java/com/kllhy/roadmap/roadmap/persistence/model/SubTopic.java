e8c852c31bd239840eb389cc184bf21f38d93ef2
한승엽 <106862797+SeungYeop-Han@users.noreply.github.com>
2025-11-09 20:09:27 +0900

    feat(topic): create entities - Topic & ResourceTopic  (#12)
    * feat(topic): create entities - Topic & ResourceTopic
    
    필드만 정의했습니다
    생성자나 메서드 등은 PR: #5 가 develop 브랜치에 머지된 뒤에
    rebase 해서 해당 방식에 맞추어 개발 진행할 계획입니다.
    
    #6
    
    * refactor(topic): move enum classes - ImportanceLevel & ResourceType
    
    enum 을 따로 모아두면 보기 편하기 때문에 리팩토링 했습니다
    
    #6
    
    * style: apply spotless format automatically
    
    * docs(topic): remove todo comments
    
    #12
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/main/java/com/kllhy/roadmap/roadmap/persistence/model/SubTopic.java
+++ src/main/java/com/kllhy/roadmap/roadmap/persistence/model/SubTopic.java
@@ -1,5 +1,6 @@
 package com.kllhy.roadmap.roadmap.persistence.model;
 
+import com.kllhy.roadmap.roadmap.persistence.model.enums.ImportanceLevel;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
