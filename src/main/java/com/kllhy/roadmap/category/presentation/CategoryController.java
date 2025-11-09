685a7eed9781d693810d467c47e3bca3fe508e5d
dd-jiyun <dlwldbs8015@gmail.com>
2025-11-09 22:19:50 +0900

    feat(category): create Entity and Repository

---
+++ src/main/java/com/kllhy/roadmap/category/presentation/CategoryController.java
@@ -0,0 +1,12 @@
+package com.kllhy.roadmap.category.presentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api-v1/category")
+public class CategoryController {
+
+    // TODO: 카테고리 전체 조회
+    // TODO: 타입별 조회
+}
