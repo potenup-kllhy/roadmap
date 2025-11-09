1f73a10db884074aec2cf35ecf1fabb049b00223
SeungYeop-Han <ammezkhan@gmail.com>
2025-11-09 17:28:12 +0900

    feat(topic): create entities - Topic & ResourceTopic
    필드만 정의했습니다
    생성자나 메서드 등은 PR: #5 가 develop 브랜치에 머지된 뒤에
    rebase 해서 해당 방식에 맞추어 개발 진행할 계획입니다.
    
    #6

---
+++ src/main/java/com/kllhy/roadmap/roadmap/persistence/model/ResourceTopic.java
@@ -0,0 +1,30 @@
+package com.kllhy.roadmap.roadmap.persistence.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "resource_topic")
+public class ResourceTopic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    // To Do: ResourceTopic.name 제약 사항 결정(ex. 길이, not null?)
+    @Column(name = "name")
+    private String name;
+
+    // To Do: 효진님 PR(#3) 머지 후 rebase 해서 ResourceType 열거형 가져와야 함, 그 전까지 커밋하지 말 것
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ResourceType resourceType;
+
+    // To Do: sort_order 제약 사항 결정(ex. 0이상? 아니면 1이상?, ...)
+    @Column(name = "sort_order", nullable = false)
+    private Integer order;
+
+    // To Do: 안전한 링크인지 확인하는 기능도 있으면 괜찮을 것 같음
+    @Column(name = "link", nullable = false)
+    private String link;
+}
