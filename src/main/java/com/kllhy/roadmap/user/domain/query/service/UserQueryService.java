45e1fff70d532b172e76b68b2bc4ec8444db2b83
hoonheori <dksxlahsl@naver,com>
2025-11-10 23:03:07 +0900

    fix(user) : typo

--- src/main/java/com/kllhy/roadmap/user/domain/query/service/UserQueryService.java
+++ src/main/java/com/kllhy/roadmap/user/domain/query/service/UserQueryService.java
@@ -1,15 +1,9 @@
 package com.kllhy.roadmap.user.domain.query.service;
 
-<<<<<<< HEAD
-import com.ohgiraffers.loadmapuser.domain.User;
-import com.ohgiraffers.loadmapuser.domain.UserRepository;
-import com.ohgiraffers.loadmapuser.domain.query.dto.UserQueryResult;
 import java.util.Optional;
-=======
 import com.kllhy.roadmap.user.domain.User;
 import com.kllhy.roadmap.user.domain.UserRepository;
 import com.kllhy.roadmap.user.domain.query.dto.UserQueryResult;
->>>>>>> 51513f5 (fix(user):typo)
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
