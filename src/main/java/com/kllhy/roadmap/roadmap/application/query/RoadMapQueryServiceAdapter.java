8163f3fd1766d3067adc6d00069c012ef332d379
youable <youable.framios@gmail.com>
2025-11-11 18:10:39 +0900

    fix(roadmap): binding RoadMap to RoadMapView Refs: #38

--- src/main/java/com/kllhy/roadmap/roadmap/application/query/RoadMapQueryServiceAdapter.java
+++ src/main/java/com/kllhy/roadmap/roadmap/application/query/RoadMapQueryServiceAdapter.java
@@ -1,7 +1,7 @@
 package com.kllhy.roadmap.roadmap.application.query;
 
 import com.kllhy.roadmap.common.exception.DomainException;
-import com.kllhy.roadmap.roadmap.application.query.dto.RoadMapView;
+import com.kllhy.roadmap.roadmap.application.query.dto.*;
 import com.kllhy.roadmap.roadmap.domain.exception.RoadMapIErrorCode;
 import com.kllhy.roadmap.roadmap.domain.model.RoadMap;
 import com.kllhy.roadmap.roadmap.domain.repository.RoadMapRepository;
@@ -22,7 +22,63 @@ public class RoadMapQueryServiceAdapter implements RoadMapQueryService {
                         .findById(id)
                         .orElseThrow(
                                 () -> new DomainException(RoadMapIErrorCode.ROAD_MAP_BAD_REQUEST));
-        return null;
+        return new RoadMapView(
+                roadMap.getId(),
+                roadMap.getTitle(),
+                roadMap.getDescription(),
+                roadMap.getDeletedAt(),
+                roadMap.getCreatedAt(),
+                roadMap.getModifiedAt(),
+                roadMap.isDeleted(),
+                roadMap.isDraft(),
+                roadMap.getCategoryId(),
+                roadMap.getTopics().stream().map(topic ->
+                    new TopicView(
+                            topic.getId(),
+                            topic.getResources().stream().map(resourceTopic ->
+                                    new ResourceTopicView(
+                                            resourceTopic.getId(),
+                                            resourceTopic.getName(),
+                                            resourceTopic.getResourceType(),
+                                            resourceTopic.getOrder(),
+                                            resourceTopic.getLink()
+                                            )
+                                    ).toList(),
+                            topic.getSubTopics().stream().map(subTopic ->
+                                    new SubTopicView(
+                                            subTopic.getId(),
+                                            subTopic.getTitle(),
+                                            subTopic.getContent(),
+                                            subTopic.getImportanceLevel(),
+                                            subTopic.getDeletedAt(),
+                                            subTopic.getIsDraft(),
+                                            subTopic.getIsDeleted(),
+                                            subTopic.getResources().stream().map(resourceSubTopic ->
+                                                    new ResourceSubTopicView(
+                                                            resourceSubTopic.getId(),
+                                                            resourceSubTopic.getName(),
+                                                            resourceSubTopic.getResourceType(),
+                                                            resourceSubTopic.getOrder(),
+                                                            resourceSubTopic.getLink()
+                                                    )
+                                            ).toList(),
+                                            subTopic.getCreatedAt(),
+                                            subTopic.getModifiedAt()
+                                    )
+
+                            ).toList(),
+                            topic.getTitle(),
+                            topic.getContent(),
+                            topic.getImportanceLevel(),
+                            topic.getOrder(),
+                            topic.getCreatedAt(),
+                            topic.getModifiedAt(),
+                            topic.getDeletedAt(),
+                            topic.isDraft(),
+                            topic.isDeleted()
+                    )
+                ).toList()
+        );
     }
 
     @Override
