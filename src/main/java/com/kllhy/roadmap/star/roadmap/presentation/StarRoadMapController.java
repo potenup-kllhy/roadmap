ed88eb2b687ecdeb4c8e3a777906f36f4265c522
dd-jiyun <dlwldbs8015@gmail.com>
2025-11-13 14:43:12 +0900

    feat(starRoadMap): add query methods to retrieve star road maps by user and roadmap Ids
    Refs: #60

--- src/main/java/com/kllhy/roadmap/star/roadmap/presentation/StarRoadMapController.java
+++ src/main/java/com/kllhy/roadmap/star/roadmap/presentation/StarRoadMapController.java
@@ -1,18 +1,24 @@
 package com.kllhy.roadmap.star.roadmap.presentation;
 
 import com.kllhy.roadmap.star.roadmap.application.command.StarRoadMapCommandService;
+import com.kllhy.roadmap.star.roadmap.application.query.StarRoadMapQueryService;
+import com.kllhy.roadmap.star.roadmap.application.query.dto.StarRoadMapView;
+import com.kllhy.roadmap.star.roadmap.presentation.exception.ErrorResponse;
 import com.kllhy.roadmap.star.roadmap.presentation.request.CreateStarRoadMapRequest;
 import com.kllhy.roadmap.star.roadmap.presentation.request.DeleteStarRoadMapRequest;
 import com.kllhy.roadmap.star.roadmap.presentation.request.UpdateStarRoadMapRequest;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StarRoadMapController {
 
     private final StarRoadMapCommandService starRoadMapCommandService;
+    private final StarRoadMapQueryService starRoadMapQueryService;
 
     @PostMapping
     public ResponseEntity<Void> createStarRoadMap(@RequestBody CreateStarRoadMapRequest request) {
@@ -29,6 +36,31 @@ public class StarRoadMapController {
                         request.userId(), request.roadmapId(), request.value());
         return ResponseEntity.created(URI.create("/api-v1/star-roadmaps/" + starRoadMapId)).build();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StarRoadMapView> getStarRoadMapById(@PathVariable Long id) {
+        var starRoadMapView = starRoadMapQueryService.getById(id);
+        return ResponseEntity.ok(starRoadMapView);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getStarRoadMaps(
+            @RequestParam(required = false) Long userId,
+            @RequestParam(required = false) Long roadmapId
+    ) {
+        if (userId != null) {
+            var starRoadMapViews = starRoadMapQueryService.getAllStarByUserId(userId);
+            return ResponseEntity.ok(starRoadMapViews);
+        }
+        if (roadmapId != null) {
+            var starRoadMapViews = starRoadMapQueryService.getAllStarByRoadMapId(roadmapId);
+            return ResponseEntity.ok(starRoadMapViews);
+        }
+
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, "Either userId or roadmapId must be provided.");
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
 
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateStarRoadMap(
