830de86170ec3b646f176a83ab1f982fa7543155
SeungYeop-Han <ammezkhan@gmail.com>
2025-11-13 16:27:46 +0900

    fix(roadMap): 로드맵 이벤트 UUID -> ID, 엔티티 생성에 대한 엔티티 이벤트 리스너 등록(누락 파일 추가)
    #64

---
+++ src/main/java/com/kllhy/roadmap/roadmap/domain/event/listener/TopicEntityListener.java
@@ -0,0 +1,22 @@
+package com.kllhy.roadmap.roadmap.domain.event.listener;
+
+import com.kllhy.roadmap.roadmap.domain.event.TopicEventOccurred;
+import com.kllhy.roadmap.roadmap.domain.event.enums.ActiveStatus;
+import com.kllhy.roadmap.roadmap.domain.event.enums.EventType;
+import com.kllhy.roadmap.roadmap.domain.model.Topic;
+import jakarta.persistence.PostPersist;
+
+public class TopicEntityListener {
+
+    @PostPersist
+    public void onPostPersist(Topic topic) {
+        TopicEventOccurred event = new TopicEventOccurred(
+                topic.getRoadMap().getId(),
+                topic.getId(),
+                EventType.CREATED,
+                topic.isDraft() ? ActiveStatus.INACTIVE : ActiveStatus.ACTIVE
+        );
+
+        SpringDomainEventPublisher.publish(event);
+    }
+}
